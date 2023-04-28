10f8aba64bba52c8c0f99d739451b6917bcfe763
Hansyo <hansyo.souti.1999@gmail.com>
2023-04-28 20:08:18 +0900

    update: 話者IDの処理を追加

--- src/dump2nemo/dump_to_nemo.py
+++ src/dump2nemo/dump_to_nemo.py
@@ -20,6 +20,7 @@ def get_args() -> argparse.Namespace:
     parser.add_argument("--dev-name", default="dev1")
     parser.add_argument("--test-name", default="test", nargs="+")
     parser.add_argument("--num_job", type=int, default=-1)
+    parser.add_argument("--with-speaker-id", action="store_true")
     args = parser.parse_args()
     return args
 
@@ -66,22 +67,50 @@ def make_wavscp_dict(line: str, espnet_dump_dir: Path, nemo_wav_dir: Path):
     return id, ext_dict
 
 
-def make_nemo_dump(espnet_dump_dir: str, nemo_wav_dir: str, data_name: str, output_dir: str, job_num: int):
+def make_speaker_dict(line: str):
+    """
+    speaker_idの情報を持った辞書型配列を作る
+    並列処理
+    """
+
+    id_speaker = line.strip().split(" ")
+    id = id_speaker[0]
+    speaker = id_speaker[1]
+    return id, {"speaker_id": speaker}
+
+
+def convert_text_id_to_number_iud(data_dict: dict, label_name: str):
+    spk_id_set = set(item[label_name] for item in [*data_dict.values()])
+    text_to_number = {text_id: number_id for number_id, text_id in enumerate(spk_id_set)}
+    return {utt_id: {label_name: text_to_number[item[label_name]]} for utt_id, item in data_dict.items()}
+
+
+def make_nemo_dump(
+    espnet_dump_dir: str,
+    nemo_wav_dir: str,
+    data_name: str,
+    output_dir: str,
+    attach_speaker: bool = False,
+    job_num: int = -1,
+):
     """
     NeMoを学習させるためのdumpファイルを作成します．
     """
 
     espnet_dump_dir = Path(espnet_dump_dir)
-    text_path = espnet_dump_dir / f"raw/{data_name}/text"
-    wavscp_path = espnet_dump_dir / f"raw/{data_name}/wav.scp"
+    text_path = espnet_dump_dir / "raw" / data_name / "text"
+    wavscp_path = espnet_dump_dir / "raw" / {data_name} / "wav.scp"
+    utt_to_spk_path = espnet_dump_dir / "raw" / {data_name} / "utt2spk"
 
     # espnet のTag機能のため
     data_name = data_name.replace("/", "_")
     output_json = Path(output_dir) / f"{data_name}_manifest.json/"
     nemo_wav_dir = Path(nemo_wav_dir)
 
-    assert text_path.exists(), f"{text_path.exists()} does not exist"
-    assert wavscp_path.exists(), f"{wavscp_path.exists()} does not exist"
+    assert text_path.exists(), f"{text_path} does not exist"
+    assert wavscp_path.exists(), f"{wavscp_path} does not exist"
+    if attach_speaker:
+        assert utt_to_spk_path.exists(), f"{utt_to_spk_path} does not exist"
 
     # textのdictを作成
     logger.info("read text file with parallel")
@@ -100,11 +129,23 @@ def make_nemo_dump(espnet_dump_dir: str, nemo_wav_dir: str, data_name: str, outp
         )
         wavscp_dicts = dict(wavscp_dicts)
 
+    if attach_speaker:
+        logger.info("read utt2spk file with parallel")
+        with open(utt_to_spk_path) as f_spk_to_txt:
+            utt_to_spk_dicts = Parallel(n_jobs=job_num)(
+                delayed(make_speaker_dict)(line, espnet_dump_dir, nemo_wav_dir)
+                for line in tqdm(f_spk_to_txt.readlines(), leave=False)
+            )
+            utt_to_spk_dicts = dict(utt_to_spk_dicts)
+        spk_to_txt_dict = convert_text_id_to_number_iud(utt_to_spk_dicts)
+
     # 集計
     dump_list = []
     for id, data in text_dicts.items():
         assert id in wavscp_dicts, f"{id} doesn't exist in text file!!!"
         data.update(wavscp_dicts[id])
+        if attach_speaker:
+            data.update(spk_to_txt_dict[id])
         dump_list.append(json.dumps(data, ensure_ascii=False) + "\n")
 
     logger.info("finish to read files. exporting....")
@@ -141,7 +182,9 @@ def main():
         nemo_wav_dir.mkdir(parents=True, exist_ok=True)
 
         # convert
-        make_nemo_dump(espnet_dump_dir, nemo_wav_dir, espnet, nemo_manifest_dir, nj)
+        make_nemo_dump(
+            espnet_dump_dir, nemo_wav_dir, espnet, nemo_manifest_dir, attach_speaker=args.with_speaker_id, job_num=nj
+        )
 
     logger.info("finished")
 
